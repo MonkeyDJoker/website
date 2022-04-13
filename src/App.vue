286660b6b8f14744aabca35bb1c3436a87a553ba
xiaosuoli <joshwjxz@gmail.com>
2022-04-13 16:02:17 +1200

    添加品牌

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,62 @@
   <button @click="stop"> 停止 </button>
 
   <p> {{msg}} </p>
+
+
+
+<div class="panel panel-primary">
+
+  <div class="panel-heading">
+    <h3 class="panel-title">Add Brand</h3>
+  </div>
+
+  <div class="panel-body form-inline">
+    <label>
+      ID:
+      <input type="text" class="form-control" v-model="id">
+    </label>
+    
+    <label>
+      Name:
+      <input type="text" class="form-control" v-model="name">
+    </label>
+
+    <input type="button" value="ADD" class="btn btn-primary" @click="add()">
+  </div>
+</div>
+
+
+<table>
+
+<thead>
+  <tr>
+    <th>ID</th>
+    <th>Name</th>
+    <th>CreateTime</th>
+    <th>Operation</th>
+  </tr>
+
+</thead>
+<tbody>
+<tr v-for="item in list" :key = "item.id">
+
+  <td>{{item.id}} </td>
+
+  <td v-text="item.name"></td>
+
+  <td>{{item.date}} </td>
+  
+  <td>
+    <a href="" @click.prevent="del(item.id)">Delete</a>
+  </td>
+
+
+</tr>
+
+</tbody>
+
+</table>
+  
 </template>
 
 <script>
@@ -18,7 +74,15 @@ export default {
     return{
       msg: '猥琐发育， 别浪！',
       count: 999,
-      internalID: null
+      internalID: null,
+      id:'',
+      name:'',
+
+      list:[
+        {id:1,name: 'james',date: new Date()},
+        {id:2,name: 'tony', date: new Date()}
+
+      ]
         }
   },
 
@@ -36,6 +100,15 @@ export default {
     stop(){
       clearInterval(this.internalID)
       this.internalID = null
+    },
+    del(id){
+      console.log(id)
+    },
+
+    add(){
+      var car = { id: this.id, name: this.name,date: new Date()}
+      this.list.push(car)
+      this.id = this.name = ''
     }
 
   }
